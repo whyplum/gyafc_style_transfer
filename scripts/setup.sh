f16f433e7d1359854c2b5772ed8fba6da8f6eca1
Inbar Oren <inbarlapid@gmail.com>
2018-11-15 00:06:32 +0200

    update scripts

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,7 +3,7 @@
 ## Lib Setup ##
 ###############
 # verify right directory
-cd text_style_transfer
+cd text_style_classification
 # install pip3
 sudo apt-get update
 sudo apt-get -y install python3-pip
@@ -12,7 +12,7 @@ pip3 install virtualenv
 python3 -m virtualenv .venv
 # activate virtualenv
 chmod +x -R .venv
-source .venv/bin/activate.sh
+source .venv/bin/activate
 # install prerequisite
 pip3 install -r requirements.txt
 # deactivate virtualenv
