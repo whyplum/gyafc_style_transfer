d7b230a14ce33d0cd73ab730d9ea6f21eac4ef5e
Inbar Oren <inbarlapid@gmail.com>
2018-11-17 11:54:36 +0200

    update train script

--- scripts/psearch.sh
+++ scripts/psearch.sh
@@ -9,12 +9,11 @@ source .venv/bin/activate
 # verify library is found
 export PYTHONPATH=$PYTHONPATH:$(pwd)/src
 # train
-python src/train.py -e 200 -b 100  -lr 0.001   -sd save/b100_lr3
-python src/train.py -e 200 -b 100  -lr 0.0001  -sd save/b100_lr4
-python src/train.py -e 200 -b 100  -lr 0.00001 -sd save/b100_lr5
-python src/train.py -e 200 -b 500  -lr 0.001   -sd save/b500_lr3
-python src/train.py -e 200 -b 500  -lr 0.0001  -sd save/b500_lr4
-python src/train.py -e 200 -b 500  -lr 0.0001  -sd save/b500_lr5
-python src/train.py -e 200 -b 1000 -lr 0.001   -sd save/b1000_lr3
-python src/train.py -e 200 -b 1000 -lr 0.0001  -sd save/b1000_lr4
-python src/train.py -e 200 -b 1000 -lr 0.0001  -sd save/b1000_lr5
+python src/train.py -e 200 -b 100  -lr 0.0001 -ru 100 -cl 50 25 10 5 -sd save/b100_lr4
+python src/train.py -e 200 -b 100  -lr 0.00001 -ru 100 -cl 50 25 10 5 -sd save/b100_lr5
+python src/train.py -e 200 -b 500  -lr 0.0001 -ru 100 -cl 50 25 10 5 -sd save/b500_lr4
+python src/train.py -e 200 -b 500  -lr 0.0001 -ru 100 -cl 50 25 10 5 -sd save/b500_lr5
+python src/train.py -e 200 -b 200 -lr 0.0001 -ru 100 -cl 50 25 10 5 -sd save/b200_lr4
+python src/train.py -e 200 -b 200 -lr 0.0001 -ru 100 -cl 50 25 10 5 -sd save/b200_lr5
+python src/train.py -e 200 -b 200 -lr 0.0001 -ru 100 -cl 30 10 -sd save/b200_lr4
+python src/train.py -e 200 -b 200 -lr 0.0001 -ru 150 -cl 50 10 -sd save/b200_lr4
